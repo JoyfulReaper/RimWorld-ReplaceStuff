8c5332ec25a3c39caba8833cecc963f3eed10399
Kyle Givler <xvegan88x@gmail.com>
2026-05-31 22:40:48 -0400

    Performance Fix
    Bail if the user isn't actively placing a blueprint.

--- Source/NewThing/CanReplaceNewThingOverOldThing.cs
+++ Source/NewThing/CanReplaceNewThingOverOldThing.cs
@@ -11,19 +11,21 @@ namespace Replace_Stuff.NewThing
 	[HarmonyPatch(typeof(GenConstruct), nameof(GenConstruct.CanReplace))]
 	class CanReplaceNewThingOverOldThing
 	{
-		// public static bool CanReplace(BuildableDef placing, BuildableDef existing, ThingDef placingStuff = null, ThingDef existingStuff = null)
 		public static void Postfix(ref bool __result, BuildableDef placing, BuildableDef existing)
 		{
-			if (((placing as ThingDef)?.IsNonDeconstructibleAttackableBuilding ?? false) || 
-			    ((existing as ThingDef)?.IsNonDeconstructibleAttackableBuilding ?? false))
+			// If the player isn't actively placing a blueprint, get out immediately.
+			// Do not cast, do not check properties, do not modify __result. Just bail.
+			if (!DesignatorContext.designating) return;
+
+			// The player IS building! Now we can safely execute the mod's normal logic.
+			if (((placing as ThingDef)?.IsNonDeconstructibleAttackableBuilding ?? false) ||
+				((existing as ThingDef)?.IsNonDeconstructibleAttackableBuilding ?? false))
 			{
 				__result = false;
 				return;
 			}
 
 			if (__result) return;
-			
-			if (!DesignatorContext.designating) return;
 
 			if (placing is ThingDef newD && existing is ThingDef oldD && newD.CanReplace(oldD))
 				__result = true;
